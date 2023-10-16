c7e0c7ecac541cd7af7fce204532b3fc44d89610
AntonelloS2001 <antonellos2001@gmail.com>
2023-10-16 10:45:06 +0200

    Aggiunti file non tracciati

---
+++ frontend/src/app/components/booking-status-dialog/booking-status-dialog.component.ts
@@ -0,0 +1,16 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-booking-status-dialog',
+  template: `
+    <h2 mat-dialog-title>{{ data.title }}</h2>
+    <mat-dialog-content>{{ data.message }}</mat-dialog-content>
+    <mat-dialog-actions>
+      <button mat-button [mat-dialog-close]="'close'">Chiudi</button>
+    </mat-dialog-actions>
+  `,
+})
+export class BookingStatusDialogComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { title: string; message: string }) {}
+}
